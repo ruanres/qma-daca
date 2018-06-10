cc1031ca4021a1fb38b61afb6aa0105f94af8d94
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2018-06-10 10:18:04 -0300

    set initial folder organization

---
+++ frontend/src/components/signin/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Signin extends Component {
+    state = {}
+    
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+        )
+    }
+}
+ 
+export default Signin;
